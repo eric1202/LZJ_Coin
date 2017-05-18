c8d2946308d1d4bf7055d0c93935177d1f5874de
eric1202 <eric19891202@126.com>
2017-05-18 16:29:44 +0800

    backup befor add pods on LZJ_ShowCoin

--- LZJ_Coin/LZJ_ShowCoin/TodayViewController.swift
+++ LZJ_Coin/LZJ_ShowCoin/TodayViewController.swift
@@ -14,14 +14,47 @@ class TodayViewController: UIViewController, NCWidgetProviding {
     @IBOutlet weak var showLbl: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        showLbl.text = "greetings!"
+        let date = Date()
+        let dateformatter = DateFormatter()
+        dateformatter.dateFormat = "MM-dd HH:mm:ss"
+        showLbl.text = "greetings!     " + dateformatter.string(from: date)
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("showing ~")
+
+        //http requset
+        DispatchQueue.global().async {
+            let url = URL(string:"https://www.okcoin.cn/api/v1/ticker.do?symbol=ltc_cny")
+            //默认的会话配置
+            let config = URLSessionConfiguration.default
+            let request = URLRequest(url: url!)
+
+            //还能设置会话超时时间，默认是1分钟，可以自己设定。
+            config.timeoutIntervalForRequest = 5 // 设置为10秒
+
+            let session = URLSession(configuration : config)
+            let task = session.dataTask(with: request, completionHandler: { (data, response, error) in
+                if data != nil{
+                    DispatchQueue.main.async {
+
+                        print(data)
+                        self.showLbl?.text = "finished" + (data?.description)!
+                        
+                    }
+                }
+            })
+
+            task.resume()
+
+            
+        }
+
+
+
     }
-    
+
     func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
         // Perform any setup necessary in order to update the view.
         
