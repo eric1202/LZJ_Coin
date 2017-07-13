9db5b12b1f707947b9105511c4378fa36e05bdf2
eric1202 <eric19891202@126.com>
2017-07-13 12:06:13 +0800

    html to plain text

--- LZJ_Coin/LZJ_Coin/Section/News/ArticleViewController.swift
+++ LZJ_Coin/LZJ_Coin/Section/News/ArticleViewController.swift
@@ -21,17 +21,30 @@ class ArticleViewController: UIViewController {
         Alamofire.request("https://interface.meiriyiwen.com/article/random?dev=1").responseJSON { (res) in
             guard let result = res.value  as? Dictionary<String,AnyObject> else{return}
 
-            let textView = UITextView.init(frame: CGRect.init(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height))
+            let textView = UITextView.init(frame: CGRect.init(x: 0, y: 64, width: self.view.frame.size.width, height: self.view.frame.size.height-64))
             textView.textColor = UIColor.white
+            textView.font = UIFont.systemFont(ofSize: 16)
+            textView.contentInset = UIEdgeInsetsMake(2, 2, 2, 2)
+            textView.isDirectionalLockEnabled = true
             textView.backgroundColor = #colorLiteral(red: 0.7254902124, green: 0.4784313738, blue: 0.09803921729, alpha: 1)
 
             guard let data :Dictionary = result["data"] as! Dictionary<String,AnyObject> else {return}
-            textView.text = data["content"] as? String
+            let t = data["content"] as? String
+            try! textView.text = t?.convertHtmlSymbols()
 
             self.title = data["title"] as? String
             self.view.addSubview(textView)
 
 
         }
+    }
+}
+
+
+extension String {
+    func convertHtmlSymbols() throws -> String? {
+        guard let data = data(using: .utf8) else { return nil }
+
+        return try NSAttributedString(data: data, options: [NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType, NSCharacterEncodingDocumentAttribute: String.Encoding.utf8.rawValue], documentAttributes: nil).string
     }
 }
